5f6e095ee303183f5bbe07b9c588dbf76b151ce1
joaquincernik <joaquincernik@gmail.com>
2024-06-07 23:41:42 -0300

    agregado de vistas viajes-disponibles y detalle viajes

--- routes/viajes.js
+++ routes/viajes.js
@@ -6,4 +6,7 @@ router.get('/viajes-disponibles/', function(req, res, next) {
   res.render('viajesDisponibles.ejs', { title: 'Express' });
 });
 
+router.get('/viajes-disponibles/viaje-detalle/', function(req, res, next) {
+  res.render('viajeDetalle.ejs', { title: 'Express' });
+});
 module.exports = router;
